a82e357f601500865e5cd2a68ac5339e29612f5a
Mutlu Dogruel <truehand@gmail.com>
2022-06-21 15:55:03 +0100

    first commit

---
+++ translate.py
@@ -0,0 +1,30 @@
+import os, requests, uuid, json
+
+# Don't forget to replace with your Translation Services key!
+subscription_key = '0123456789'
+
+# Our Flask route will supply two arguments: text_input and language_output.
+# When the translate text button is pressed in our Flask app, the Ajax request
+# will grab these values from our web app, and use them in the request.
+def get_translation(text_input, language_output):
+    base_url = 'https://api.cognitive.microsofttranslator.com'
+    path = '/translate?api-version=3.0'
+    params = '&to=' + language_output
+    constructed_url = base_url + path + params
+
+    headers = {
+        'Ocp-Apim-Subscription-Key': subscription_key,
+        'Ocp-Apim-Subscription-Region': 'global',
+        'Content-type': 'application/json',
+        'X-ClientTraceId': str(uuid.uuid4())
+    }
+
+    # You can pass more than one object in body.
+    body = [{
+        'text' : text_input
+    }]
+    response = requests.post(constructed_url, headers=headers, json=body)
+    #print (text_input)
+    #print (language_output)
+    #print (response.json())
+    return response.json()
